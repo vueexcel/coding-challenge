fd69bc5efc731c208aa58764fe67eb04391d7657
vueexcel <vue.excel2011@gmail.com>
2022-01-20 19:04:41 +0530

    created btn component

---
+++ src/components/Btn.vue
@@ -0,0 +1,13 @@
+<template>
+  <button :class="btnStyle">{{btnText}}</button>
+</template>
+
+<script>
+export default {
+  name:'btn',
+  props: {
+      btnStyle: String,
+      btnText: String
+  }
+}
+</script>
